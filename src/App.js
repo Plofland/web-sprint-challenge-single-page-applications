b78585d1e38d4884953986d0b2179feb2c6d1916
Plofland <ploflan@gmail.com>
2020-12-18 13:55:11 -0500

    Fixed form error, passed down props to form, and added initial values and slice of state for values, errors, and disabled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import Form from "./components/Form";
 
+const initialPizzFormValues = {
+  pizzaSize: "",
+  sauceChoice: "",
+  pepperoni: false,
+  dicedTomatoes: false,
+  sausage: false,
+  blackOlives: false,
+  canadianBacon: false,
+  roastedGarlic: false,
+  spicyItalianSausage: false,
+  artichokeHearts: false,
+  grilledChicken: false,
+  threeCheese: false,
+  onions: false,
+  pineapple: false,
+  greenPepper: false,
+  extraCheese: false,
+  specialInstructions: "",
+};
+
+const initialPizzaFormErrors = {
+  pizzaSize: "",
+  sauceChoice: "",
+};
+
+initialDisabled = true;
+
 const App = () => {
+  const [pizzaFormValues, setPizzaFormValues] = useState(
+    initialPizzaFormValues,
+  );
+  const [formErrors, setFormErrors] = useState(initialPizzaFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
   return (
     <div>
       <nav>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,6 @@ import React from "react";
 import pizzaImg from "./Assets/Pizza.jpg";
 
 export default function Form(props) {
-
   const { values, submit, change, disabled, errors } = props;
 
   const onSubmit = (event) => {
@@ -17,120 +16,184 @@ export default function Form(props) {
   };
 
   return (
-    <form className="containter" onSubmit={onSubmit}>
-      <div className="formHeader">
-        <h2>Lambda Eats</h2>
-        <div className="linkBtn">
-          <a href="/app.js">Home</a>
+    <div>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <Link to="/">Home</Link>
+        <Link to="/">Help</Link>
+      </nav>
+      <form className="containter" onSubmit={onSubmit}>
+        {/* <div className="formHeader">
+          <h2>Lambda Eats</h2>
+          <div className="linkBtn">
+            <a href="/app.js">Home</a>
+          </div>
+          <div className="linkBtn">
+            <a href="/">Help</a>
+          </div>
+        </div> */}
+        <div className="pizzaBuilder">
+          <h3 id="byopH3">Build Your Own Pizza</h3>
+          <img id="formImg" src="Pizza.jpg" alt="pizza" />
+          <h2 id="byopH2">Build Your Own Pizza</h2>
+          <label className="inputTitle">
+            Choice of Size
+            <div>{errors.sizeErr}</div>
+            <select className="dropDown" name="pizzaSize">
+              <option value="">-Select-</option>
+              <option value="small">Small</option>
+              <option value="medium">Medium</option>
+              <option value="large">Large</option>
+            </select>
+          </label>
+          <label className="inputTitle">
+            Choice of Sauce
+            <div>{errors.sauceErr}</div>
+            <div>
+              <input type="radio" name="sauceChoice" value="redSauce">
+                Original Red
+              </input>
+              <input type="radio" name="sauceChoice" value="garlicSauce">
+                Garlic Ranch
+              </input>
+              <input type="radio" name="sauceChoice" value="bbqSauce">
+                BBQ Sauce
+              </input>
+              <input type="radio" name="sauceChoice" value="spinSauce">
+                Spinach Alfredo
+              </input>
+            </div>
+          </label>
+          <label className="inputTitle">
+            Add Toppings
+            <div>{errors.toppingsErr}</div>
+            <div className="toppingCheckboxes">
+              <input
+                type="checkbox"
+                name="pepperoni"
+                checked={values.pepperoni}
+              >
+                Pepperoni
+              </input>
+              <input
+                type="checkbox"
+                name="dicedTomatoes"
+                checked={values.dicedTomatoes}
+              >
+                Diced Tomatoes
+              </input>
+              <input type="checkbox" name="sausage" checked={values.sausage}>
+                Sausage
+              </input>
+              <input
+                type="checkbox"
+                name="blackOlives"
+                checked={values.blackOlives}
+              >
+                Black Olives
+              </input>
+              <input
+                type="checkbox"
+                name="canadianBacon"
+                checked={values.canadianBacon}
+              >
+                Canadian Bacon
+              </input>
+              <input
+                type="checkbox"
+                name="roastedGarlic"
+                checked={values.roastedGarlic}
+              >
+                Roasted Garlic
+              </input>
+              <input
+                type="checkbox"
+                name="spicyItalianSausage"
+                checked={values.spicyItalianSausage}
+              >
+                Spicy Italian Sausage
+              </input>
+              <input
+                type="checkbox"
+                name="artichokeHearts"
+                checked={values.artichokeHearts}
+              >
+                Artichoke Hearts
+              </input>
+              <input
+                type="checkbox"
+                name="grilledChicken"
+                checked={values.grilledChicken}
+              >
+                Grilled Chicken
+              </input>
+              <input
+                type="checkbox"
+                name="threeCheese"
+                checked={values.threeCheese}
+              >
+                Three Cheese
+              </input>
+              <input type="checkbox" name="onions" checked={values.onions}>
+                Onions
+              </input>
+              <input
+                type="checkbox"
+                name="pineapple"
+                checked={values.pineapple}
+              >
+                Pineapple
+              </input>
+              <input
+                type="checkbox"
+                name="greenPepper"
+                checked={values.greenPepper}
+              >
+                Green Pepper
+              </input>
+              <input
+                type="checkbox"
+                name="extraCheese"
+                checked={values.extraCheese}
+              >
+                Extra Cheese
+              </input>
+            </div>
+          </label>
+          {/* <label className="inputTitle switch">
+            Choice of Substitute
+            <div>{errors.substituteErr}</div>
+            <input type="checkbox">Gluten Free Crust (+ $1.00)</input>
+            <span class="slider"></span>
+          </label> */}
+          <label className="inputTitle">
+            Special Instructions
+            <input
+              type="text"
+              name="specialInstructions"
+              placeholder="Anything else you'd like to add?"
+            ></input>
+          </label>
         </div>
-        <div className="linkBtn">
-          <a href="/">Help</a>
-        </div>
-      </div>
-      <div className="pizzaBuilder">
-        <h3 id="byopH3">Build Your Own Pizza</h3>
-        <img id="formImg" src="Pizza.jpg" alt="pizza" />
-        <h2 id="byopH2">Build Your Own Pizza</h2>
-        <label className="inputTitle">
-          Choice of Size
-          <div>{errors.sizeErr}</div>
-          <select className="dropDown" name='pizzaSize'>
-            <option value="">-Select-</option>
-            <option value="small">Small</option>
-            <option value="medium">Medium</option>
-            <option value="large">Large</option>
+        <div>
+          <select type="" name="quantity">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
           </select>
-        </label>
-        <label className="inputTitle">
-          Choice of Sauce
-          <div>{errors.sauceErr}</div>
-          <div>
-            <input type="radio" name="sauceChoice" value="redSauce">Original Red</input>
-            <input type="radio" name="sauceChoice" value="garlicSauce">Garlic Ranch</input>
-            <input type="radio" name="sauceChoice" value="bbqSauce">BBQ Sauce</input>
-            <input type="radio" name="sauceChoice" value="spinSauce">Spinach Alfredo</input>
-          </div>
-        </label>
-        <label className="inputTitle">
-          Add Toppings
-          <div>{errors.toppingsErr}</div>
-          <div className="toppingCheckboxes">
-            <input type="checkbox" name="toppings" checked={values.pepperoni}>
-              Pepperoni
-            </input>
-            <input type="checkbox" name="toppings" checked={values.dicedTomatoes}>
-              Diced Tomatoes
-            </input>
-            <input type="checkbox" name="toppings" checked={values.sausage}>
-              Sausage
-            </input>
-            <input type="checkbox" name="toppings" checked={values.blackOlives}>
-              Black Olives
-            </input>
-            <input type="checkbox" name="toppings" checked={values.canadianBacon}>
-              Canadian Bacon
-            </input>
-            <input type="checkbox" name="toppings" checked={values.roastedGarlic}>
-              Roasted Garlic
-            </input>
-            <input type="checkbox" name="toppings" checked={values.spicyItalianSausage">
-              Spicy Italian Sausage
-            </input>
-            <input type="checkbox" name="toppings" checked={values.artichokeHearts}>
-              Artichoke Hearts
-            </input>
-            <input type="checkbox" name="toppings" checked={values.grilledChicken}>
-              Grilled Chicken
-            </input>
-            <input type="checkbox" name="toppings" checked={values.threeCheese}>
-              Three Cheese
-            </input>
-            <input type="checkbox" name="toppings" checked={values.onions}>
-              Onions
-            </input>
-            <input type="checkbox" name="toppings" checked={values.pineapple}>
-              Pineapple
-            </input>
-            <input type="checkbox" name="toppings" checked={values.greenPepper}>
-              Green Pepper
-            </input>
-            <input type="checkbox" name="toppings" checked={values.extraCheese}>
-              Extra Cheese
-            </input>
-          </div>
-        </label>
-        {/* <label className="inputTitle switch">
-          Choice of Substitute
-          <div>{errors.substituteErr}</div>
-          <input type="checkbox">Gluten Free Crust (+ $1.00)</input>
-          <span class="slider"></span>
-        </label> */}
-        <label className="inputTitle">
-          Special Instructions
-          <input
-            type="text"
-            placeholder="Anything else you'd like to add?"
-          ></input>
-        </label>
-      </div>
-      <div>
-        <select type="" name="quantity">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-        <button type="submit" disabled={disabled}>
-          Add to Order
-        </button>
-      </div>
-    </form>
+          <button type="submit" disabled={disabled}>
+            Add to Order
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
